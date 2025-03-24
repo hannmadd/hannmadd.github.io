4f2d331cea1f28bd4d4db42db4e9d1b47a8213f8
hannmadd <himaddon22@gmail.com>
2025-03-24 17:58:35 -0400

    COMPONENTS DIOR

---
+++ components/script.js
@@ -0,0 +1,7 @@
+function openMenu() {
+    document.getElementById("mobileNav").style.display = "block";
+}
+
+function closeMenu() {
+    document.getElementById("mobileNav").style.display = "none";
+}
